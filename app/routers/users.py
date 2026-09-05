a24d2e6fcd796ee2145b3895a91268383407dcef
rk.dev <rekubota.dev@gmail.com>
2026-09-05 15:46:37 +0900

    feat: ユーザー情報の更新と削除機能を追加

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,14 +1,62 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
+from app.database import get_db
 from app.dependencies import get_current_user
 from app.models.user import User
-from app.schemas.user import UserResponse
+from app.schemas.user import UserResponse, UserUpdate
+from app.security import password_hash
 
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
+# GET_ME
 @router.get("/me", response_model=UserResponse)
 def get_me(current_user: User = Depends(get_current_user)):
     return current_user
+
+
+# PATCH
+@router.patch("/me", response_model=UserResponse)
+def update_me(
+    user_data: UserUpdate,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    # emailが送られてきた場合
+    if user_data.email is not None:
+
+        existing_user = db.query(User).filter(
+                User.email == user_data.email,
+                User.id != current_user.id,
+            ).first()
+
+        if existing_user is not None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered"
+            )
+
+        current_user.email = user_data.email
+
+    # passwordが送られてきた場合
+    if user_data.password is not None:
+        current_user.hashed_password = password_hash.hash(user_data.password)
+
+    db.commit()
+    db.refresh(current_user)
+
+    return current_user
+
+
+# DELETE
+@router.delete("/me", status_code=status.HTTP_204_NO_CONTENT)
+def delete_me(
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    db.delete(current_user)
+    db.commit()
+
+    return None
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -7,6 +7,11 @@ class UserCreate(BaseModel):
     password: str
     
     
+class UserUpdate(BaseModel):
+    email: EmailStr | None = None
+    password: str | None = None
+    
+    
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
